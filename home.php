c19d3799e9787f77f39e7fbeca9f85579945f3b6
EnjinArchives <159727700+EnjinArchives@users.noreply.github.com>
2024-02-11 15:38:16 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Enjin Archives</h1></br>
+
+  This is a site dedicated to the preservation of information about Enjin.com forums and its users.
+
+  
 This is a main page that has not been written yet. Since I have more or less forgotten all my HTML for now, it's not gonna have any formatting.</br>
 </br>
-<b> Except I'll try this. </b> <a href=enjin.com>This is the broken link to Enjin.com which is now fucked because fuck them.</a>
+<font=b> Except I'll try this. </b> <a href=enjin.com>This is the broken link to Enjin.com which is now fucked because fuck them.</a>
+
+
+</body>
+</html>
