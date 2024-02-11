990d2ef3bd1fb75d474f9fdedb8f59f4fcbf3018
EnjinArchives <159727700+EnjinArchives@users.noreply.github.com>
2024-02-11 15:42:44 -0500

    Update home.php

--- home.php
+++ home.php
@@ -9,7 +9,9 @@
   
 This is a main page that has not been written yet. Since I have more or less forgotten all my HTML for now, it's not gonna have any formatting.</br>
 </br>
-<font=b> Except I'll try this. </b> <a href=http://enjin.com>This is the broken link to Enjin.com which is now fucked because fuck them.</a>
+<font=b> Except I'll try this. </b> </font> </br></br>
+  
+  <a href=http://enjin.com>As you can see, the site enjin.com now redirects to the bullshit cuntfest that is their crypto "business".</a> So, it's totally lost. Apart from the archives.
 
 
 </body>
