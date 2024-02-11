cf9a9b7681416003873b9f15cca7f5c261310eea
EnjinArchives <159727700+EnjinArchives@users.noreply.github.com>
2024-02-11 15:40:48 -0500

    Update home.php

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
   
 This is a main page that has not been written yet. Since I have more or less forgotten all my HTML for now, it's not gonna have any formatting.</br>
 </br>
-<font=b> Except I'll try this. </b> <a href=enjin.com>This is the broken link to Enjin.com which is now fucked because fuck them.</a>
+<font=b> Except I'll try this. </b> <a href=http://enjin.com>This is the broken link to Enjin.com which is now fucked because fuck them.</a>
 
 
 </body>
