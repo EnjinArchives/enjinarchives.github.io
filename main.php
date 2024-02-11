f60f9f547c6dd3b884243dfcda232ea3d87b8e81
EnjinArchives <159727700+EnjinArchives@users.noreply.github.com>
2024-02-11 15:36:27 -0500

    Update and rename main.php to home.php
    </br> added

--- main.php
+++
@@ -1,3 +0,0 @@
-This is a main page that has not been written yet. Since I have more or less forgotten all my HTML for now, it's not gonna have any formatting.
-
-<b> Except I'll try this. </b> <a href=enjin.com>This is the broken link to Enjin.com which is now fucked because fuck them.</a>
